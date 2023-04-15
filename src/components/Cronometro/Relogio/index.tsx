a0cb6a71f6c2cd33673c36e925ac974f667be4a3
Débora Reis <125418483+deboranreis@users.noreply.github.com>
2023-04-15 10:41:24 -0300

    Ajuste do bug do cronometro

--- src/components/Cronometro/Relogio/index.tsx
+++ src/components/Cronometro/Relogio/index.tsx
@@ -6,12 +6,19 @@ interface Props {
 }
 
 export default function Relogio({ tempo = 0 }: Props) {
-    const minutos = Math.floor(tempo / 60);
+    const horas = Math.floor(tempo / 3600);
+    const minutos = Math.floor((tempo % 3600) / 60);
     const segundos = tempo % 60;
+    const [horasUnidade, horasDezena] = String(horas).padStart(2, '0');
     const [minutoDezena, minutoUnidade] = String(minutos).padStart(2, '0');
     const [segundoDezena, segundoUnidade] = String (segundos).padStart(2, '0');
     return (
         <React.Fragment>
+            <div className={style.relogioNumero}>
+                <span>{horasUnidade}</span>
+            </div>
+            <span className={style.relogioNumero}>{horasDezena}</span>
+            <span className={style.relogioDivisao}>:</span>
             <span className={style.relogioNumero}>{minutoDezena}</span>
             <span className={style.relogioNumero}>{minutoUnidade}</span>
             <span className={style.relogioDivisao}>:</span>
